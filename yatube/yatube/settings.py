83bd82b5f93c693738871a84cd631b019d1d9caf
Andrei Lebedev <keni-kin@yandex.ru>
2022-09-06 00:05:33 +0300

    final

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-env_path = os.path.join(os.path.curdir, ".env")
+env_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")
 load_dotenv(dotenv_path=env_path)
 
 SECRET_KEY = os.getenv("SECRET_KEY")
